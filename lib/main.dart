a70edfa926c7faaa3682a66df838665337d4461d
Kabiru Mwenja <makabby@gmail.com>
2019-08-05 20:52:03 +0300

    chore: Add external lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,12 +6,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(title: Text('Welcome to Flutter')),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asCamelCase),
         ),
       ),
     );
